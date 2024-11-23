8333e507a12d3360522236288e7f8a61ef51ef6f
danodoms <danodoms@gmail.com>
2024-11-23 11:02:13 +0800

    model prediction now runs async

--- app/(tabs)/scan.tsx
+++ app/(tabs)/scan.tsx
@@ -1,45 +1,41 @@
-import {useState, useEffect, useRef} from 'react';
-import {loadTensorflowModel, TensorflowModel} from "react-native-fast-tflite";
+import { useState, useEffect, useRef } from "react";
+import { loadTensorflowModel, TensorflowModel } from "react-native-fast-tflite";
 import {
-    Camera,
-    useCameraDevice,
-    useCameraPermission,
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
 } from "react-native-vision-camera";
-import {Image, View} from 'react-native'
+import { Image, View } from "react-native";
 /*import {useResizePlugin} from "vision-camera-resize-plugin";*/
-import {VStack} from "@/components/ui/vstack";
-import { Text } from '@/components/ui/text';
-import {Worklets} from "react-native-worklets-core";
-import {plantDiseaseClasses} from "@/assets/model/tflite/plant-disease/plant-disease-classes";
-import * as ImageManipulator from 'expo-image-manipulator';
-import {convertToRGB} from "react-native-image-to-rgb";
+import { VStack } from "@/components/ui/vstack";
+import { Text } from "@/components/ui/text";
+import { Worklets } from "react-native-worklets-core";
+import { plantDiseaseClasses } from "@/assets/model/tflite/plant-disease/plant-disease-classes";
+import * as ImageManipulator from "expo-image-manipulator";
+import { convertToRGB } from "react-native-image-to-rgb";
 import {
-    Button,
-    ButtonText,
-    ButtonSpinner,
-    ButtonIcon,
-    ButtonGroup,
-} from '@/components/ui/button';
-import {SaveFormat} from "expo-image-manipulator";
-import {Box} from "@/components/ui/box";
+  Button,
+  ButtonText,
+  ButtonSpinner,
+  ButtonIcon,
+  ButtonGroup,
+} from "@/components/ui/button";
+import { SaveFormat } from "expo-image-manipulator";
+import { Box } from "@/components/ui/box";
 import ScanResultDrawer from "@/components/ScanResultDrawer";
 
-
 export default function ScanScreen() {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const [isTfReady, setIsTfReady] = useState(false);
-    const [classification, setClassification] = useState<string  | null>(null);
-    const [confidence, setConfidence] = useState<string | null>(null);
-    const [model, setModel] = useState<TensorflowModel | null>(null);
-    const cameraRef = useRef<Camera | null>(null);
-    const [capturedImageUri, setCapturedImageUri] = useState<string | null>(null); // To hold the image URI
-    /*const { resize } = useResizePlugin()*/
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isTfReady, setIsTfReady] = useState(false);
+  const [classification, setClassification] = useState<string | null>(null);
+  const [confidence, setConfidence] = useState<string | null>(null);
+  const [model, setModel] = useState<TensorflowModel | null>(null);
+  const cameraRef = useRef<Camera | null>(null);
+  const [capturedImageUri, setCapturedImageUri] = useState<string | null>(null); // To hold the image URI
+  /*const { resize } = useResizePlugin()*/
 
-
-    const { hasPermission, requestPermission } = useCameraPermission()
-    const device = useCameraDevice('back')
-
-
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const device = useCameraDevice("back");
 
   /*  const runClassification = Worklets.createRunOnJS((outputs:{}) => {
         const result = getMaxClassification(outputs, plantDiseaseClasses)
@@ -48,37 +44,34 @@ export default function ScanScreen() {
         setClassification(`${result.className} (Score: ${result.maxValue.toFixed(6)})`); // Optionally include the score
     })*/
 
+  const performClassification = (outputs: {}) => {
+    const result = getMaxClassification(outputs, plantDiseaseClasses);
 
-      const performClassification = ((outputs:{}) => {
-          const result = getMaxClassification(outputs, plantDiseaseClasses)
+    setConfidence(result.maxValue.toFixed(6));
 
-          setConfidence(result.maxValue.toFixed(6))
+    // Update classification state
+    setClassification(result.className);
+  };
 
-          // Update classification state
-          setClassification(`${result.className} (Score: ${result.maxValue.toFixed(6)})`); // Optionally include the score
-      })
+  const getMaxClassification = (outputs, outputClasses: object) => {
+    // Find the key of the highest value
+    const maxKey = Object.keys(outputs).reduce((a, b) =>
+      outputs[a] > outputs[b] ? a : b
+    );
 
-    const getMaxClassification = (outputs, outputClasses:object) => {
-        // Find the key of the highest value
-        const maxKey = Object.keys(outputs).reduce((a, b) =>
-            outputs[a] > outputs[b] ? a : b
-        );
+    // Get the highest value
+    const maxValue = outputs[maxKey];
 
-        // Get the highest value
-        const maxValue = outputs[maxKey];
+    // Get the disease name using the key
+    const className = outputClasses[maxKey];
 
-        // Get the disease name using the key
-        const className = outputClasses[maxKey];
-
-        return {
-            className,
-            maxValue
-        };
+    return {
+      className,
+      maxValue,
     };
+  };
 
-
-
-/*    const frameProcessor = useFrameProcessor((frame) => {
+  /*    const frameProcessor = useFrameProcessor((frame) => {
         'worklet'
         /!*console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`)*!/
         if (model == null) return console.log("no model loaded")
@@ -110,173 +103,208 @@ export default function ScanScreen() {
         })
     }, [model])*/
 
-
-
-
-
-
-    // Ensure TensorFlow is ready before classifying
-    useEffect(() => {
-        const initializeTf = async () => {
-            // await tf.ready();
-            setIsTfReady(true);
-            await loadModel();  // Load the model when TensorFlow is ready
-        };
-        initializeTf();
-    }, []);
-
-    const loadModel = async () => {
-        // Load the TFLite model from the app bundle
-        /*   const tfliteModel = await loadTensorflowModel(require("../../../assets/model/tflite/ASL.tflite"))*/
-        const tfliteModel = await loadTensorflowModel(require("../../assets/model/tflite/plant-disease/plant-disease.tflite"))
-        setModel(tfliteModel);
+  // Ensure TensorFlow is ready before classifying
+  useEffect(() => {
+    const initializeTf = async () => {
+      // await tf.ready();
+      setIsTfReady(true);
+      await loadModel(); // Load the model when TensorFlow is ready
     };
+    initializeTf();
+  }, []);
 
+  const loadModel = async () => {
+    // Load the TFLite model from the app bundle
+    /*   const tfliteModel = await loadTensorflowModel(require("../../../assets/model/tflite/ASL.tflite"))*/
+    const tfliteModel = await loadTensorflowModel(
+      require("../../assets/model/tflite/plant-disease/plant-disease.tflite")
+    );
+    setModel(tfliteModel);
+  };
 
+  if (!hasPermission) {
+    return (
+      <VStack>
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission}>
+          <ButtonText>grant permission</ButtonText>
+        </Button>
+      </VStack>
+    );
+  }
 
-    if (!hasPermission) {
-        return (
-            <VStack>
-                <Text>We need your permission to show the camera</Text>
-                <Button onPress={requestPermission}>
-                    <ButtonText>
-                        grant permission
-                    </ButtonText>
-                </Button>
-            </VStack>
-        );
-    }
+  if (!device) {
+    return (
+      <VStack>
+        <Text>No camera device</Text>
+      </VStack>
+    );
+  }
 
-    if (!device) {
-        return (
-            <VStack>
-                <Text>No camera device</Text>
-            </VStack>
-        );
-    }
-
-    /*function toggleCameraFacing() {
+  /*function toggleCameraFacing() {
         setFacing(current => (current === 'back' ? 'front' : 'back'));
     }*/
 
+  const captureAndClassify = async () => {
+    if (!model) {
+      console.log("Model is not loaded yet.");
+      return;
+    }
+    if (!cameraRef.current) {
+      console.log("No camera ref");
+      return;
+    }
 
-    const captureAndClassify = async () => {
-        if (!model) return console.log("Model is not loaded yet.");
-        if (!cameraRef.current) return console.log("no camera ref");
+    try {
+      // Capture the image from the camera
+      const photo = await cameraRef.current.takePhoto();
 
-        // Capture the image from the camera
-        const photo = await cameraRef.current.takePhoto();
+      if (!photo) {
+        throw new Error("Photo is undefined, no image captured");
+      }
+      console.log("Image Captured");
 
-        if (!photo) {
-            throw new Error("Photo is undefined, no image captured");
-        }
-        console.log("Image Captured");
+      // Reset the states and show the drawer
+      setConfidence(null);
+      setCapturedImageUri(null);
+      setClassification(null);
+      setIsDrawerOpen(true);
 
+      // Resize the image to fit the model requirements
+      const manipulatedImage = await ImageManipulator.manipulateAsync(
+        "file://" + photo.path,
+        [{ resize: { width: 224, height: 224 } }],
+        { format: SaveFormat.JPEG, base64: true }
+      );
+      console.log("Image Resized");
 
-        // Resize the image to fit the model requirements ex. 224 x 224 in 3 channels
-        const manipulatedImage = await ImageManipulator.manipulateAsync("file://"  +  photo.path, [{ resize: { width: 224, height: 224 } }], {format:SaveFormat.JPEG , base64:true});
-        console.log("Image Resized")
+      setCapturedImageUri("file://" + manipulatedImage.uri);
 
-        // Sets the captured image preview to flash on screen
-        setCapturedImageUri("file://" + manipulatedImage.uri); // Set the image URI to show on screen
-        setTimeout(() => setCapturedImageUri(null), 5000); // Hide the image preview after 5 seconds
+      // Convert image into correct format
+      convertImageToRgb(manipulatedImage.uri, "float32")
+        .then((imageTensor) => {
+          console.log("Starting Classification");
+          return model.run([imageTensor]);
+        })
+        .then((prediction) => {
+          console.log(prediction);
+          return performClassification(prediction[0]);
+        })
+        .then(() => {
+          console.log("Done, Image Classified");
+          setIsDrawerOpen(true);
+        })
+        .catch((error) => {
+          console.error("Error during classification:", error);
+        });
+    } catch (error) {
+      console.error("Error in capture and classify:", error);
+    }
+  };
 
-        //convert image into correct format
-        const imageTensor = await convertImageToRgb(manipulatedImage.uri, 'float32')
+  const convertImageToRgb = async (image, format: "uint8" | "float32") => {
+    const convertedArray = await convertToRGB(image);
+    console.log("convertedArray:", convertedArray);
 
+    return new Promise((resolve, reject) => {
+      try {
+        // Process in chunks to avoid blocking the main thread
+        let red: number[] = [];
+        let green: number[] = [];
+        let blue: number[] = [];
+        const chunkSize = 1000; // Adjust based on performance needs
 
-        // Perform classification with the loaded TFLite model
-        console.log("Starting Classification");
-        const prediction = await model.run([imageTensor])
-        console.log(prediction)
-        setIsDrawerOpen(true)
-
-        await performClassification(prediction[0])
-
-
-        console.log("Done, Image Classified")
-    };
-
-
-
-    type imageFormat = 'uint8' | 'float32'
-    const convertImageToRgb = async (image,format:imageFormat='uint8') => {
-        const convertedArray = await convertToRGB(image);
-        console.log("convertedArray:", convertedArray);
-
-        let red:number[] = []
-        let blue:number[] = []
-        let green:number[] = []
-
-        // This normalizes the RGB values by dividing 255
-        for (let index = 0; index < convertedArray.length; index += 3) {
+        const processChunk = (startIndex: number) => {
+          for (
+            let index = startIndex;
+            index < Math.min(startIndex + chunkSize, convertedArray.length);
+            index += 3
+          ) {
             red.push(convertedArray[index] / 255);
             green.push(convertedArray[index + 1] / 255);
             blue.push(convertedArray[index + 2] / 255);
-        }
-        const finalArray = [...red, ...green, ...blue];
+          }
 
-        console.log('normalized array: ', finalArray);
+          if (startIndex + chunkSize < convertedArray.length) {
+            // Continue processing the next chunk
+            setTimeout(() => processChunk(startIndex + chunkSize), 0);
+          } else {
+            // Finalize and resolve the promise
+            const finalArray = [...red, ...green, ...blue];
+            console.log("normalized array: ", finalArray);
 
-        //convert to array buffer (some models require uint8 or float32 format)
-        if (format === 'float32') {
-            return new Float32Array(finalArray);
-        } else if (format === 'uint8') {
-            return new Uint8Array(finalArray);
-        } else {
-            throw new Error("Invalid format specified. Use 'float32' or 'uint8'.");
-        }
-    };
+            if (format === "float32") {
+              resolve(new Float32Array(finalArray));
+            } else if (format === "uint8") {
+              resolve(new Uint8Array(finalArray));
+            } else {
+              reject(
+                new Error("Invalid format specified. Use 'float32' or 'uint8'.")
+              );
+            }
+          }
+        };
 
+        processChunk(0); // Start processing
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
 
+  return (
+    <VStack className="bg-green p-4 outline-red-500 outline outline-1 h-full relative">
+      <Camera
+        device={device}
+        style={{
+          position: "absolute", // This makes the component absolutely positioned
+          top: 0, // Adjust these values as needed
+          left: 0,
+          right: 0,
+          bottom: 0,
+        }}
+        isActive={true}
+        ref={cameraRef}
+        photo={true}
+        /*  frameProcessor={frameProcessor}*/
+      />
 
-
-
-    return (
-        <VStack className="bg-green p-4 outline-red-500 outline outline-1 h-full relative">
-
-
-                <Camera
-                    device={device}
-                     style={{
-                         position: 'absolute', // This makes the component absolutely positioned
-                         top: 0,              // Adjust these values as needed
-                         left: 0,
-                         right: 0,
-                         bottom: 0,
-                     }}
-                    isActive={true}
-                    ref={cameraRef}
-                    photo={true}
-                    /*  frameProcessor={frameProcessor}*/
-                />
-
-
-
-
-
-            {/* Image Preview */}
-            {capturedImageUri && (
-                <VStack>
-                    <Image source={{ uri: capturedImageUri }} />
-                </VStack>
-            )}
-
-            {/* Display Classification Result */}
-            {classification && (
-                <Text>
-                    {classification}
-                </Text>
-            )}
-
-
-            <Button onPress={captureAndClassify} disabled={!isTfReady} size="lg" variant="solid" action="primary" className="rounded-lg">
-                {isTfReady ? <ButtonText>Classify</ButtonText> : <ButtonText>Waiting for Tensorflow...</ButtonText>}
-            </Button>
-
-            <ScanResultDrawer drawerState={{isDrawerOpen,setIsDrawerOpen, imageUri:capturedImageUri, classification, confidence}} />
+      {/* Image Preview */}
+      {capturedImageUri && (
+        <VStack>
+          <Image source={{ uri: capturedImageUri }} />
         </VStack>
-    );
+      )}
+
+      {/* Display Classification Result */}
+      {classification && <Text>{classification}</Text>}
+
+      <Button
+        onPress={captureAndClassify}
+        disabled={!isTfReady}
+        size="lg"
+        variant="solid"
+        action="primary"
+        className="rounded-lg"
+      >
+        {isTfReady ? (
+          <ButtonText>Classify</ButtonText>
+        ) : (
+          <ButtonText>Waiting for Tensorflow...</ButtonText>
+        )}
+      </Button>
+
+      <ScanResultDrawer
+        drawerState={{
+          isDrawerOpen,
+          setIsDrawerOpen,
+          imageUri: capturedImageUri,
+          classification,
+          confidence,
+        }}
+      />
+    </VStack>
+  );
 }
 
 /*const styles = StyleSheet.create({
--- components/ScanResultDrawer.tsx
+++ components/ScanResultDrawer.tsx
@@ -1,82 +1,96 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import {
-    Drawer,
-    DrawerBackdrop,
-    DrawerContent,
-    DrawerHeader,
-    DrawerCloseButton,
-    DrawerBody,
-    DrawerFooter,
-} from "@/components/ui/drawer"
-import {Button,ButtonText} from "@/components/ui/button";
-import {Heading} from "@/components/ui/heading";
-import {Text} from "@/components/ui/text";
-import { Image } from '@/components/ui/image';
-import {Skeleton} from "@/components/ui/skeleton";
-
-
-
+  Drawer,
+  DrawerBackdrop,
+  DrawerContent,
+  DrawerHeader,
+  DrawerCloseButton,
+  DrawerBody,
+  DrawerFooter,
+} from "@/components/ui/drawer";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
+import { Image } from "@/components/ui/image";
+import { Skeleton, SkeletonText } from "@/components/ui/skeleton";
+import { VStack } from "@/components/ui/vstack";
+import { Box } from "@/components/ui/box";
 
 interface ScanResultDrawerProps {
-    drawerState: {
-        isDrawerOpen: boolean;
-        setIsDrawerOpen: (open: boolean) => void;
-        imageUri: string | null;
-        classification: string | null;
-        confidence: string | null;
-    };
+  drawerState: {
+    isDrawerOpen: boolean;
+    setIsDrawerOpen: (open: boolean) => void;
+    imageUri: string | null;
+    classification: string | null;
+    confidence: string | null;
+  };
 }
 
-
 const ScanResultDrawer: React.FC<ScanResultDrawerProps> = ({ drawerState }) => {
-    return (
-        <>
-            <Drawer
-                isOpen={drawerState.isDrawerOpen}
-                /*isOpen={true}*/
-                onClose={() => {
-                    drawerState.setIsDrawerOpen(false)
+  return (
+    <>
+      <Drawer
+        isOpen={drawerState.isDrawerOpen}
+        /*isOpen={true}*/
+        onClose={() => {
+          drawerState.setIsDrawerOpen(false);
+        }}
+        size="md"
+        anchor="bottom"
+      >
+        <DrawerBackdrop />
+        <DrawerContent>
+          <DrawerHeader>
+            {drawerState.classification ? (
+              <Heading size="2xl" className="text-center">
+                {drawerState.classification}
+              </Heading>
+            ) : (
+              /*<VStack space="md" className="h-20">
+                <Skeleton variant="rounded" className="h-4 w-full" />
+
+                <Skeleton variant="rounded" className="h-4 w-1/2" />
+              </VStack>*/
+
+              <SkeletonText _lines={2} className="h-4 rounded-md" />
+            )}
+          </DrawerHeader>
+          <DrawerBody>
+            {drawerState.confidence ? (
+              <Text size="xl" className="text-typography-400 mb-4">
+                Confidence: {drawerState.confidence}
+              </Text>
+            ) : (
+              <Skeleton variant="rounded" className="h-4 w-1/2 mb-4" />
+            )}
+
+            {drawerState.imageUri ? (
+              <Image
+                size="2xl"
+                className="min-h-full min-w-full rounded-md"
+                alt="classification-image"
+                source={{
+                  uri: drawerState.imageUri,
                 }}
-                size="sm"
-                anchor="bottom"
+              ></Image>
+            ) : (
+              <Skeleton variant="rounded" className="h-full w-full" />
+            )}
+          </DrawerBody>
+          <DrawerFooter>
+            <Button
+              onPress={() => {
+                drawerState.setIsDrawerOpen(false);
+              }}
+              className="flex-1"
             >
-                <DrawerBackdrop />
-                <DrawerContent>
-                    <DrawerHeader>
-                        <Heading size="2xl" className="text-center">{drawerState.classification}</Heading>
-                        <Skeleton variant="rounded" className="h-20 w-full" />
-                    </DrawerHeader>
-                    <DrawerBody>
-                        <Text size="xl" className="text-typography-800">
-                            {drawerState.confidence}
-                        </Text>
-
-
-                        {drawerState.imageUri ?
-                        <Image size="md" className="w-full" alt="classification-image" source={{
-                            uri: drawerState.imageUri
-                        }}>
-                        </Image>
-                         :
-                            <Skeleton variant="rounded" className="h-20 w-full" />
-                        }
-
-
-                    </DrawerBody>
-                    <DrawerFooter>
-                        <Button
-                            onPress={() => {
-                                drawerState.setIsDrawerOpen(false)
-                            }}
-                            className="flex-1"
-                        >
-                            <ButtonText>Button</ButtonText>
-                        </Button>
-                    </DrawerFooter>
-                </DrawerContent>
-            </Drawer>
-        </>
-    );
+              <ButtonText>Close</ButtonText>
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
 };
 
 export default ScanResultDrawer;
